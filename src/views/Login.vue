1200c04607d5b25791c57a7d706ee016e080df3f
gustavo.silva <gustavo.silva@codificar.com.br>
2021-01-30 16:06:20 -0300

    Validate Form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,70 +2,101 @@
     <div style="height: 300px;">
         <LoginMenu />
 
-        <div
-            v-if="response.message"
-            :class="`rounded-sm bg-${response.color}-100 p-4 mb-4`"
+        <ValidationObserver
+            tag="form"
+            ref="loginForm"
+            @submit.stop.prevent="login"
         >
-            <h3 :class="`text-sm leading-5 font-medium text-${response.color}-800`">
-                {{ response.message }}
-            </h3>
-        </div>
-
-        <div class="grid gap-2">
-            <input
-                v-model="email"
-                type="text"
-                placeholder="Digite seu e-mail"
-                class="bg-gray-900 placeholder-gray-700 text-gray-500 font-light border border-gray-900 focus:outline-none focus:border-blue-800 rounded-sm py-3 px-4 block w-full appearance-none leading-normal"
+            <div
+                v-if="response.message"
+                :class="`rounded-sm bg-${response.color}-100 p-4 mb-4`"
             >
-
-
-
-            <input
-                v-model="password"
-                type="password"
-                placeholder="Digite sua senha"
-                class="bg-gray-900 placeholder-gray-700 text-gray-500 font-light border border-gray-900 focus:outline-none focus:border-blue-800 rounded-sm py-3 px-4 block w-full appearance-none leading-normal"
-            >
-
-
-
-            <button
-                type="submit"
-                @click.stop.prevent="login()"
-                :disabled="spinner.login"
-                class="flex items-center justify-center bg-blue-800 text-blue-200 font-medium text-sm focus:outline-none rounded-sm py-3 px-4 block w-full appearance-none leading-normal"
-            >
-                <img
-                    v-if="spinner.login"
-                    src="@/assets/img/spinner.svg"
-                    alt=""
-                    class="w-5 h-5 mr-2"
-                >
-
-                ENTRAR
-            </button>
-
-            <div class="my-4 text-center">
-                <RouterLink
-                    :to="{ name: 'forgotPassword' }"
-                    class="text-sm font-light"
-                >
-                    Esqueci minha senha
-                </RouterLink>
+                <h3 :class="`text-sm leading-5 font-medium text-${response.color}-800`">
+                    {{ response.message }}
+                </h3>
             </div>
-        </div>
+
+            <div class="grid gap-2">
+                <ValidationProvider
+                    v-slot="{ errors }"
+                    rules="required|email"
+                    name="E-mail"
+                >
+                    <input
+                        autofocus
+                        v-model="email"
+                        type="text"
+                        placeholder="Digite seu e-mail"
+                        class="bg-gray-900 placeholder-gray-700 text-gray-500 font-light border border-gray-900 focus:outline-none focus:border-blue-800 rounded-sm py-3 px-4 block w-full appearance-none leading-normal"
+                    >
+                    <div
+                        v-if="!!errors[0]"
+                        class="text-red-500 text-sm mb-2"
+                    >
+                        {{ errors[0] }}
+                    </div>
+                </ValidationProvider>
+
+                <ValidationProvider
+                    v-slot="{ errors }"
+                    rules="required"
+                    name="Senha"
+                >
+                    <input
+                        autocomplete
+                        v-model="password"
+                        type="password"
+                        placeholder="Digite sua senha"
+                        class="bg-gray-900 placeholder-gray-700 text-gray-500 font-light border border-gray-900 focus:outline-none focus:border-blue-800 rounded-sm py-3 px-4 block w-full appearance-none leading-normal"
+                    >
+                    <div
+                        v-if="!!errors[0]"
+                        class="text-red-500 text-sm mb-2"
+                    >
+                        {{ errors[0] }}
+                    </div>
+                </ValidationProvider>
+
+
+                <button
+                    type="submit"
+                    :disabled="spinner.login"
+                    class="flex items-center justify-center bg-blue-800 text-blue-200 font-medium text-sm focus:outline-none rounded-sm py-3 px-4 block w-full appearance-none leading-normal"
+                >
+                    <img
+                        v-if="spinner.login"
+                        src="@/assets/img/spinner.svg"
+                        alt=""
+                        class="w-5 h-5 mr-2"
+                    >
+
+                    ENTRAR
+                </button>
+
+                <div class="my-4 text-center">
+                    <RouterLink
+                        :to="{ name: 'forgotPassword' }"
+                        class="text-sm font-light"
+                    >
+                        Esqueci minha senha
+                    </RouterLink>
+                </div>
+            </div>
+        </ValidationObserver>
     </div>
 </template>
 
 <script>
-    import LoginMenu from '../components/Auth/LoginMenu.vue';
+    import LoginMenu from '@/components/Auth/LoginMenu.vue';
     import Cookie from 'js-cookie';
+    import { ValidationObserver, ValidationProvider } from 'vee-validate';
 
     export default {
         name: 'Login',
 
         components: {
+            ValidationProvider,
+            ValidationObserver,
             LoginMenu,
         },
 
@@ -85,6 +116,7 @@
 
         methods: {
             async login() {
+                if(!await this.$refs.loginForm.validate()) return false;
                 const payload = {
                     email: this.email,
                     password: this.password,
