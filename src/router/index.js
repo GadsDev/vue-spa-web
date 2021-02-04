b7fd01cb67e190f6011dd1915ac48faac7a71d57
gustavo.silva <gustavo.silva@codificar.com.br>
2021-02-03 22:19:38 -0300

    Implement First Middware redirectIfAuthenticated

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,31 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import LayoutAuth from '../layouts/Auth.vue';
+import LayoutDefault from '../layouts/Default.vue';
+
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import VerifyEmail from '@/views/VerifyEmail';
 import ForgotPassword from '@/views/ForgotPassword';
 import ResetPassword from '@/views/ResetPassword';
 
+import Home from '@/views/Home';
+
+import Guard from '@/service/middleware';
+
 Vue.use(VueRouter);
 
 const routes = [
     {
+        path: '/', component: LayoutDefault,
+        //beforeEnter:
+        children: [
+            { path: '', name: 'index', component: Home },
+        ],
+    },
+    {
         path: '/login', component: LayoutAuth,
+        beforeEnter: Guard.redirectIfAuthenticated,
         children: [
             { path: '', name: 'login', component: Login },
         ],
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+        <div class="flex items-center justify-between mb-8">
+            <div class="text-gray-500 font-medium">
+                Lista de tarefas
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'Home',
+
+        data() {
+            return {
+            };
+        },
+
+        created() {
+
+        },
+
+        methods: {
+
+        },
+    };
+</script>
